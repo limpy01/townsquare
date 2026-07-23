cd0ddcce456a39abe0345f441dec2811bdc1aae9
timelic <i@timel.ink>
2026-07-24 03:46:45 +0800

    refactor: migrate role modal to composition API

--- src/components/modals/RoleModal.vue
+++ src/components/modals/RoleModal.vue
@@ -12,7 +12,12 @@
     <ul class="tokens" v-if="tab === 'editionRoles' || !otherTravelers.size">
       <li
         v-for="role in availableRoles"
-        v-show="(!role.id && !role.name) || ['townsfolk', 'outsider', 'minion', 'demon'].includes(role.team) || (role.team == 'traveler' && (!session.isSpectator || isShowTraveler && playerIndex < 0))"
+        v-show="
+          (!role.id && !role.name) ||
+          ['townsfolk', 'outsider', 'minion', 'demon'].includes(role.team) ||
+          (role.team == 'traveler' &&
+            (!session.isSpectator || (isShowTraveler && playerIndex < 0)))
+        "
         :class="[role.team]"
         :key="role.id"
         @click="setRole(role)"
@@ -21,10 +26,18 @@
         <Token :role="role" />
       </li>
     </ul>
-    <ul class="tokens" v-if="tab === 'editionRolesFull' || !otherTravelers.size">
+    <ul
+      class="tokens"
+      v-if="tab === 'editionRolesFull' || !otherTravelers.size"
+    >
       <li
         v-for="role in availableRoles"
-        v-show="(!role.id && !role.name) || ['townsfolk', 'outsider', 'minion', 'demon'].includes(role.team) || (role.team == 'traveler' &&  (!session.isSpectator || isShowTraveler && playerIndex < 0))"
+        v-show="
+          (!role.id && !role.name) ||
+          ['townsfolk', 'outsider', 'minion', 'demon'].includes(role.team) ||
+          (role.team == 'traveler' &&
+            (!session.isSpectator || (isShowTraveler && playerIndex < 0)))
+        "
         :class="[role.team]"
         :key="role.id"
         @click="setRole(role)"
@@ -52,20 +65,18 @@
         class="button"
         :class="{ townsfolk: tab === 'editionRoles' }"
         @click="tab = 'editionRoles'"
-        >
+      >
         <span v-if="playerIndex >= 0">剧本角色</span>
         <span v-else>不在场角色</span>
-        </span
-      >
+      </span>
       <span
         v-if="playerIndex < 0"
         class="button"
         :class="{ townsfolk: tab === 'editionRolesFull' }"
         @click="tab = 'editionRolesFull'"
-        >
-        全部角色
-        </span
       >
+        全部角色
+      </span>
       <span
         v-if="playerIndex >= 0"
         class="button"
@@ -82,100 +93,84 @@
       <span>显示旅行者</span> &nbsp;
       <em>
         <font-awesome-icon
-          :icon="[
-            'fas',
-            isShowTraveler ? 'check-square' : 'square'
-          ]"
+          :icon="['fas', isShowTraveler ? 'check-square' : 'square']"
         />
       </em>
     </div>
   </Modal>
 </template>
 
-<script>
-import { mapMutations, mapState } from "vuex";
-import Modal from "./Modal";
-import Token from "../Token";
+<script setup lang="ts">
+import { computed, onBeforeUnmount, onMounted, ref } from "vue";
+import Modal from "./Modal.vue";
+import Token from "../Token.vue";
+import store from "../../store";
+import { useModalStore } from "../../stores/modals";
+import { usePlayersStore } from "../../stores/players";
+import { useScenarioStore } from "../../stores/scenario";
+import { useSessionIdentityStore } from "../../stores/session-identity";
 
-export default {
-  components: { Token, Modal },
-  props: ["playerIndex"],
-  computed: {
-    availableRoles() {
-      const availableRoles = [];
-      const players = this.$store.state.players.players;
-      this.$store.state.roles.forEach(role => {
-        // don't show bluff roles that are already assigned to players
-        if (
-          this.tab === "editionRolesFull" ||
-          this.playerIndex >= 0 ||
-          (this.playerIndex < 0 &&
-            !players.some(player => player.role.id === role.id))
-        ) {
-          availableRoles.push(role);
-        }
-      });
-      availableRoles.push({});
-      return availableRoles;
-    },
-    tokenWidth() {
-      const percentage = 0.06
-      const width = percentage * this.windowWidth;
-      return width >= 80 ? "width: 6vw" : "width: 80px";
-    },
-    ...mapState(["modals", "roles", "session"]),
-    ...mapState("players", ["players"]),
-    ...mapState(["otherTravelers"])
-  },
-  data() {
-    return {
-      tab: "editionRoles",
-      isShowTraveler: false,
-      windowWidth: window.innerWidth,
-      windowHeight: window.innerHeight
-    };
-  },
-  mounted(){
-    window.addEventListener("resize", this.handleResize);
-  },
-  beforeUnmount(){
-    window.removeEventListener("resize", this.handleResize);
-  },
-  methods: {
-    handleResize(){
-      this.windowWidth = window.innerWidth;
-      this.windowHeight = window.innerHeight;
-    },
-    setRole(role) {
-      if (this.playerIndex < 0) {
-        // assign to bluff slot (index < 0)
-        this.$store.commit("players/setBluff", {
-          index: this.playerIndex * -1 - 1,
-          role
-        });
-      } else {
-        if (this.session.isSpectator && role.team === "traveler") return;
-        // assign to player
-        const player = this.$store.state.players.players[this.playerIndex];
-        this.$store.commit("players/update", {
-          player,
-          property: "role",
-          value: role
-        });
-      }
-      this.tab = "editionRoles";
-      this.$store.commit("toggleModal", "role");
-    },
-    close() {
-      this.tab = "editionRoles";
-      this.toggleModal("role");
-    },
-    toggleShowTraveler() {
-      this.isShowTraveler = !this.isShowTraveler;
-    },
-    ...mapMutations(["toggleModal"])
+const { playerIndex } = defineProps<{ playerIndex: number }>();
+const modals = useModalStore();
+const playerState = usePlayersStore();
+const scenario = useScenarioStore();
+const session = useSessionIdentityStore();
+const players = computed(() => playerState.players);
+const roles = scenario.roles as Map<string, any>;
+const otherTravelers = scenario.otherTravelers as Map<string, any>;
+const tab = ref("editionRoles");
+const isShowTraveler = ref(false);
+const windowWidth = ref(window.innerWidth);
+const windowHeight = ref(window.innerHeight);
+
+const availableRoles = computed(() => {
+  const available = Array.from(roles.values()).filter(
+    (role) =>
+      tab.value === "editionRolesFull" ||
+      playerIndex >= 0 ||
+      !players.value.some((player) => player.role.id === role.id),
+  );
+  return [...available, {}];
+});
+
+const tokenWidth = computed(() =>
+  windowWidth.value * 0.06 >= 80 ? "width: 6vw" : "width: 80px",
+);
+
+onMounted(() => window.addEventListener("resize", handleResize));
+onBeforeUnmount(() => window.removeEventListener("resize", handleResize));
+
+function handleResize() {
+  windowWidth.value = window.innerWidth;
+  windowHeight.value = window.innerHeight;
+}
+
+function setRole(role: any) {
+  if (playerIndex < 0) {
+    store.commit("players/setBluff", {
+      index: playerIndex * -1 - 1,
+      role,
+    });
+  } else {
+    if (session.isSpectator && role.team === "traveler") return;
+    store.commit("players/update", {
+      player: players.value[playerIndex],
+      property: "role",
+      value: role,
+    });
   }
-};
+  tab.value = "editionRoles";
+  modals.toggle("role");
+}
+
+function close() {
+  tab.value = "editionRoles";
+  modals.toggle("role");
+}
+
+function toggleShowTraveler() {
+  isShowTraveler.value = !isShowTraveler.value;
+}
 </script>
 
 <style scoped lang="scss">
@@ -188,19 +183,29 @@ ul.tokens li {
   transition: transform 500ms ease;
 
   &.townsfolk {
-    box-shadow: 0 0 10px $townsfolk, 0 0 10px #004cff;
+    box-shadow:
+      0 0 10px $townsfolk,
+      0 0 10px #004cff;
   }
   &.outsider {
-    box-shadow: 0 0 10px $outsider, 0 0 10px $outsider;
+    box-shadow:
+      0 0 10px $outsider,
+      0 0 10px $outsider;
   }
   &.minion {
-    box-shadow: 0 0 10px $minion, 0 0 10px $minion;
+    box-shadow:
+      0 0 10px $minion,
+      0 0 10px $minion;
   }
   &.demon {
-    box-shadow: 0 0 10px $demon, 0 0 10px $demon;
+    box-shadow:
+      0 0 10px $demon,
+      0 0 10px $demon;
   }
   &.traveler {
-    box-shadow: 0 0 10px $traveler, 0 0 10px $traveler;
+    box-shadow:
+      0 0 10px $traveler,
+      0 0 10px $traveler;
   }
   &:hover {
     transform: scale(1.2);
@@ -209,18 +214,17 @@ ul.tokens li {
 }
 
 .check-box {
-  display: flex; 
-  justify-content: center; 
+  display: flex;
+  justify-content: center;
   align-items: center;
-  width: fit-content; 
+  width: fit-content;
 
   margin-left: auto;
   margin-right: auto;
-  
-  cursor: pointer; 
+
+  cursor: pointer;
   &:hover {
     color: red;
   }
 }
-
 </style>
